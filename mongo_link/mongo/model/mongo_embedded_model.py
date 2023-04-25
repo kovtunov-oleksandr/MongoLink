2351c9f0bc3203b0170cd84cca23a0a692b7c3c1
Kovtunov Oleksandr <kovtunov.oleksandr.00@gmail.com>
2023-04-25 22:59:52 +0300

    Provides config for MongoEmbeddedModel

--- mongo_link/mongo/model/mongo_embedded_model.py
+++ mongo_link/mongo/model/mongo_embedded_model.py
@@ -1,5 +1,13 @@
+from bson import ObjectId
 from pydantic import BaseModel
 
 
 class MongoEmbeddedModel(BaseModel):
     """Don't use id field"""
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
